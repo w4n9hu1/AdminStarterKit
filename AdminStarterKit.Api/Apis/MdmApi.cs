c4d213059a33747a6e797617126aa0940cb8f5d0
w4n9hu1 <wanghui89757@gmail.com>
2024-03-08 09:42:14 +0800

    add MdmServices

--- AdminStarterKit.Api/Apis/MdmApi.cs
+++ AdminStarterKit.Api/Apis/MdmApi.cs
@@ -1,7 +1,6 @@
 ﻿using AdminStarterKit.Api.Contracts;
 using AdminStarterKit.Api.Validations;
 using AdminStarterKit.Domain.Aggregates;
-using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AdminStarterKit.Api.Apis
@@ -10,30 +9,30 @@ namespace AdminStarterKit.Api.Apis
     {
         public static RouteGroupBuilder MapMdmApi(this RouteGroupBuilder builder)
         {
-            builder.MapPost("/user", async Task<CommonApiResponse<UserDto>> ([FromBody] CreateUserRequest request, [FromServices] IServiceProvider sp) =>
-            {
-                var validator = new CreateUserRequestValidator();
-                var validationResult = await validator.ValidateAsync(request);
-                if (validationResult.IsValid == false)
-                {
-                    return CommonApiResponse<UserDto>.Failed(validationResult.ToString());
-                }
-                var user = new User
-                {
-                    Email = request.Email,
-                    UserName = request.UserName,
-                    PasswordHash = request.Password,
-                    CreatedDateTime = DateTimeOffset.UtcNow,
-                    UpdatedDateTime = DateTimeOffset.UtcNow
-                };
-                var userRepository = sp.GetRequiredService<IUserRepository>();
-                userRepository.Add(user);
-                await userRepository.UnitOfWork.SaveChangesAsync();
-                var mapper = sp.GetRequiredService<IMapper>();
-                var userDto = mapper.Map<UserDto>(user);
-                return CommonApiResponse<UserDto>.Success(userDto);
-            });
+            builder.MapPost("/user", CreateUser);
             return builder;
+        }
+
+        public static async Task<CommonApiResponse<UserDto>> CreateUser([FromBody] CreateUserRequest request, [AsParameters] MdmServices services)
+        {
+            var validator = new CreateUserRequestValidator();
+            var validationResult = await validator.ValidateAsync(request);
+            if (validationResult.IsValid == false)
+            {
+                return CommonApiResponse<UserDto>.Failed(validationResult.ToString());
+            }
+            var user = new User
+            {
+                Email = request.Email,
+                UserName = request.UserName,
+                PasswordHash = request.Password,
+                CreatedDateTime = DateTimeOffset.UtcNow,
+                UpdatedDateTime = DateTimeOffset.UtcNow
+            };
+            services.UserRepository.Add(user);
+            await services.UserRepository.UnitOfWork.SaveChangesAsync();
+            var userDto = services.Mapper.Map<UserDto>(user);
+            return CommonApiResponse<UserDto>.Success(userDto);
         }
     }
 }
---
+++ AdminStarterKit.Api/Apis/MdmServices.cs
@@ -0,0 +1,13 @@
+﻿using AdminStarterKit.Domain.Aggregates;
+using AutoMapper;
+
+namespace AdminStarterKit.Api.Apis
+{
+    public class MdmServices(
+        IMapper mapper,
+        IUserRepository userRepository)
+    {
+        public IMapper Mapper { get; set; } = mapper;
+        public IUserRepository UserRepository { get; set; } = userRepository;
+    }
+}
